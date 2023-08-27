6f2a3389781611bece0ae6d73de9d9d39bf4e752
Michel Domingos <micheldslive@gmail.com>
2023-08-26 23:50:55 -0300

    feat: creating mocks for stories

---
+++ src/core/mocks/index.ts
@@ -0,0 +1,14 @@
+export const CONTACT_INFO = {
+  email: 'johndoe@example.com',
+  name: 'John Doe',
+  phone: '81995593520',
+  position: 'Job Position',
+}
+
+export const FORM_INFO = {
+  address: 'Address example',
+  contact: CONTACT_INFO,
+  id: '1',
+  title: 'Job Title',
+  editing: true,
+}
